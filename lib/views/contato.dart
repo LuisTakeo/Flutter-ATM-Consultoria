5080a0907ea903f5a6c6d0a01d844d9d862d813e
LuisTakeo <luistakeo@hotmail.com>
2024-04-10 22:08:48 -0300

    Criado página de contato

--- lib/views/contato.dart
+++ lib/views/contato.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Contato extends StatefulWidget {
   const Contato({Key? key}) : super(key: key);
@@ -10,6 +12,44 @@ class Contato extends StatefulWidget {
 class _ContatoState extends State<Contato> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contato"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset("img/detalhe_cliente.png"),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text("Clientes"),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Image.asset("img/cliente1.png"),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text("Empresa de Software"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Image.asset("img/cliente2.png"),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text("Empresa de Software"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
